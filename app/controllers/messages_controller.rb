63afd47e189eb9ae4294291a1ca0c6e617ff706f
wakanamatsumoto <wakana_matsumoto@glocalize.co.jp>
2016-06-05 13:27:36 +0000

    8.7まで完了

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,28 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [ :edit, :update, :destroy ]
+  
+  def edit
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+
+  def update
+    if @message.update(message_params)
+      #保存に成功した場合はアップデート
+      redirect_to root_path , notice:'メッセージを編集しました'
+    else
+      #保存に失敗した場合は編集画面に戻す
+      render 'edit'
+    end
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
   def index
     @message = Message.new
     # Messageを全て取得する。
