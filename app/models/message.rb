299db3704cdeaa197e099541a574e63de06eda0f
wakanamatsumoto <wakana_matsumoto@glocalize.co.jp>
2016-06-06 15:21:59 +0000

    Add age column

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
     validates :name , length:{ maximum: 20 }, presence:true
     #内容入力は必須かつ２文字以上30文字以下
     validates :body , length:{ minimum: 20, maximum: 30 }, presence:true
+    #年齢のカラムは0以上
+    validates :age , numericality: { greater_than: 0 }, presence:true
 end
